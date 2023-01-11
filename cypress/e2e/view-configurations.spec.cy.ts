0b2939564640322140ac6acdfa32e60a2cbbc410
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-01-11 10:01:53 +0200

    Fixes configuration of control bar and add tests to test this scenario.

--- cypress/e2e/view-configurations.spec.cy.ts
+++ cypress/e2e/view-configurations.spec.cy.ts
@@ -13,108 +13,139 @@ describe('View configurations', () => {
       cy.clearLocalStorage('prefixes');
    });
 
-   it('Should render all tabs by default', () => {
-      // GIVEN: "view-configurations" page is visited.
+   describe('View mode', () => {
 
-      // THEN: all components of yasgui, yasr, yasqe have to be visible.
-      YasqeSteps.getQueryTabs().should('be.visible');
-      YasrSteps.getResultHeader().should('be.visible');
+      it('Should render all tabs by default', () => {
+         // GIVEN: "view-configurations" page is visited.
+
+         // THEN: all components of yasgui, yasr, yasqe have to be visible.
+         YasqeSteps.getQueryTabs().should('be.visible');
+         YasrSteps.getResultHeader().should('be.visible');
+      });
+
+      it('Should hide query tabs', () => {
+         // GIVEN: "view-configurations" page is visited.
+
+         // WHEN: set configuration property "showEditorTabs" to false.
+         ViewConfigurationsPageSteps.hideEditorTabs();
+
+         // THEN: only query tabs have to be invisible.
+         YasqeSteps.getQueryTabs().should('not.be.visible');
+         YasrSteps.getResultHeader().should('be.visible');
+
+         // WHEN: set configuration property "showEditorTabs" to true.
+         ViewConfigurationsPageSteps.showEditorTabs();
+
+         // THEN: all components of yasgui, yasr, yasqe have to be visible.
+         YasqeSteps.getQueryTabs().should('be.visible');
+         YasrSteps.getResultHeader().should('be.visible');
+      });
+
+      it('Should hide result tabs', () => {
+         // GIVEN: "view-configurations" page is visited.
+
+         // WHEN: set configuration property "showResultTabs" to false.
+         ViewConfigurationsPageSteps.hideResultTabs();
+
+         // THEN: only result tabs have to be invisible.
+         YasqeSteps.getQueryTabs().should('be.visible');
+         YasrSteps.getResultHeader().should('not.be.visible');
+
+         // WHEN: set configuration property "showResultTabs" to true.
+         ViewConfigurationsPageSteps.showResultTabs();
+
+         // THEN: all components of yasgui, yasr, yasqe have to be visible.
+         YasqeSteps.getQueryTabs().should('be.visible');
+         YasrSteps.getResultHeader().should('be.visible');
+      });
    });
 
-   it('Should hide query tabs', () => {
-      // GIVEN: "view-configurations" page is visited.
+   describe('Query configurations', () => {
 
-      // WHEN: set configuration property "showEditorTabs" to false.
-      ViewConfigurationsPageSteps.hideEditorTabs();
+      it('Should set query passed through "initialQuery" configuration', () => {
+         // GIVEN: "view-configurations" page is visited.
 
-      // THEN: only query tabs have to be invisible.
-      YasqeSteps.getQueryTabs().should('not.be.visible');
-      YasrSteps.getResultHeader().should('be.visible');
+         // WHEN: set 'initial_query' value to configuration "initialQuery"
+         ViewConfigurationsPageSteps.setInitialQuery();
 
-      // WHEN: set configuration property "showEditorTabs" to true.
-      ViewConfigurationsPageSteps.showEditorTabs();
+         // THEN: 'initial_query' have to be set in the query editor.
+         YasqeSteps.getEditor().contains('initial_query');
+      });
 
-      // THEN: all components of yasgui, yasr, yasqe have to be visible.
-      YasqeSteps.getQueryTabs().should('be.visible');
-      YasrSteps.getResultHeader().should('be.visible');
+      it('Should set default query', () => {
+         // GIVEN: "view-configurations" page is visited.
+
+         // WHEN: set 'default_query' value to configuration "query".
+         ViewConfigurationsPageSteps.setDefaultQuery();
+         // AND: open a new tab.
+         YasguiSteps.openANewTab();
+
+         // THEN: 'default_query' have to be set in the query editor.
+         YasqeSteps.getEditor().contains('default_query');
+      });
+
+      it('Should fire event when query is executed', () => {
+         // GIVEN: "view-configurations" page is visited.
+
+         // WHEN: execute an query
+         YasqeSteps.executeQuery();
+
+         // THEN: an event "queryExecuted" have to be fired
+         // AND: a new element in dom have to be added.
+         ViewConfigurationsPageSteps.getQueryRanInfo().should('be.visible');
+      });
    });
 
-   it('Should hide result tabs', () => {
-      // GIVEN: "view-configurations" page is visited.
+   describe('Control bar', () => {
 
-      // WHEN: set configuration property "showResultTabs" to false.
-      ViewConfigurationsPageSteps.hideResultTabs();
+      it('Should control bar not be visible with default configuration', () => {
+         // WHEN: "view-configurations" page is visited.
 
-      // THEN: only result tabs have to be invisible.
-      YasqeSteps.getQueryTabs().should('be.visible');
-      YasrSteps.getResultHeader().should('not.be.visible');
+         // THEN: an event "queryExecuted" have to be fired
+         // AND: a new element in dom have to be added.
+         YasqeSteps.getControlBar().should('not.be.visible');
+      });
 
-      // WHEN: set configuration property "showResultTabs" to true.
-      ViewConfigurationsPageSteps.showResultTabs();
+      it('Should control bar be visible', () => {
+         // When I configure control panel to be visible.
+         ViewConfigurationsPageSteps.showControlBar();
 
-      // THEN: all components of yasgui, yasr, yasqe have to be visible.
-      YasqeSteps.getQueryTabs().should('be.visible');
-      YasrSteps.getResultHeader().should('be.visible');
-   });
+         // Then I expected control panel to not be visible.
+         YasqeSteps.getControlBar().should('be.visible');
 
-   it('Should set query passed through "initialQuery" configuration', () => {
-      // GIVEN: "view-configurations" page is visited.
+      });
 
-      // WHEN: set 'initial_query' value to configuration "initialQuery"
-      ViewConfigurationsPageSteps.setInitialQuery();
+      it('Should control bar not be visible', () => {
+         // When I configure control panel to be visible.
+         ViewConfigurationsPageSteps.showControlBar();
 
-      // THEN: 'initial_query' have to be set in the query editor.
-      YasqeSteps.getEditor().contains('initial_query');
-   });
+         // And I change configuration of control panel to not be visible.
+         ViewConfigurationsPageSteps.hideControlBar();
 
-   it('Should set default query', () => {
-      // GIVEN: "view-configurations" page is visited.
+         // Then I expected control panel to be visible.
+         YasqeSteps.getControlBar().should('not.be.visible');
+      });
 
-      // WHEN: set 'default_query' value to configuration "query".
-      ViewConfigurationsPageSteps.setDefaultQuery();
-      // AND: open a new tab.
-      YasguiSteps.openANewTab();
+      it("Should control bar not be visible when is turned of and new tab is opened", () => {
+         // When control bar is hidden
+         ViewConfigurationsPageSteps.hideControlBar();
 
-      // THEN: 'default_query' have to be set in the query editor.
-      YasqeSteps.getEditor().contains('default_query');
-   });
+         // And I open a new query tab
+         YasguiSteps.openANewTab();
 
-   it('Should fire event when query is executed', () => {
-      // GIVEN: "view-configurations" page is visited.
+         // Then I expect control bar to be not visible in the new tab
+         YasqeSteps.getControlBar().should('not.be.visible');
+      });
 
-      // WHEN: execute an query
-      YasqeSteps.executeQuery();
+      it("Should control bar not be visible when is turned of and new tab is opened", () => {
+         // When control bar is configured to be enabled.
+         ViewConfigurationsPageSteps.showControlBar();
 
-      // THEN: an event "queryExecuted" have to be fired
-      // AND: a new element in dom have to be added.
-      ViewConfigurationsPageSteps.getQueryRanInfo().should('be.visible');
-   });
+         // And I open a new query tab
+         YasguiSteps.openANewTab();
 
-   it('Should control bar not be visible with default configuration', () => {
-      // WHEN: "view-configurations" page is visited.
-
-      // THEN: an event "queryExecuted" have to be fired
-      // AND: a new element in dom have to be added.
-      YasqeSteps.getControlBar().should('not.be.visible');
-   });
-
-   it('Should control bar be visible', () => {
-      // When I configure control panel to be visible.
-      ViewConfigurationsPageSteps.showControlBar();
-
-      // Then I expected control panel to not be visible.
-      YasqeSteps.getControlBar().should('be.visible');
-
-   });
-
-   it('Should control bar not be visible', () => {
-      // When I configure control panel to be visible.
-      ViewConfigurationsPageSteps.showControlBar();
-
-      // And I change configuration of control panel to not be visible.
-      ViewConfigurationsPageSteps.hideControlBar();
-
-      // Then I expected control panel to be visible.
-      YasqeSteps.getControlBar().should('not.be.visible');
+         // Then I expect control bar to be visible in the new tab
+         YasqeSteps.getControlBar().should('be.visible');
+      });
    });
 });
