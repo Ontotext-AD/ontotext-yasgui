bf9b04b28d040c66761505a8b88876d77808f88c
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-01-23 18:13:12 +0200

    GDB-7711: Extends table render plugin
    ## What?
    - Added styling to results in table plugin.
    - Added custom message about count of result, time of execution and when the query was executed.
    - Implement cypress.
    ## Why?
    GDB workbench has custom styling of table plugin results and custom result info message.
    ## How?
     - Extends Yasr and overrides function "updateResponseInfo".
    - Implement tests.
    Additional work:
     - Added formatting of default query.

--- cypress/stubs/query-stubs.ts
+++ cypress/stubs/query-stubs.ts
@@ -1,5 +1,13 @@
 export class QueryStubs {
     static stubDefaultQueryResponse(withDelay: number = 0) {
-        cy.intercept('/repositories/test-repo', {fixture: '/queries/default-query-response.json', delay: withDelay}).as('getGuides');
+        QueryStubs.stubQueryResponse('/queries/default-query-response.json', 'getDefaultQueryResponse', withDelay);
+    }
+
+    static stubEmptyQueryResponse(withDelay: number = 0) {
+        QueryStubs.stubQueryResponse('/queries/empty-query-response.json', 'getEmptyResponse', withDelay);
+    }
+
+    static stubQueryResponse(fixture: string, alias: string, withDelay: number = 0) {
+        cy.intercept('/repositories/test-repo', {fixture, delay: withDelay}).as(alias);
     }
 }
