3642ad77a7dfa2690ca3d86a433c8a69695da828
Svilen Velikov <svilen.velikov@ontotext.com>
2023-02-12 16:39:41 +0200

    GDB-7890 register and fire needed events from every opened editor tab
    ## What
    When an action is executed in the editor, like executing a query an event should be emitted to outside for the client to be able to catch and do its job. This must happen for every editor tab, be it existent or new one.
    
    ## Why
    A client needs to reliably receive events from any editor tab which is currently active.
    
    ## How
    * Subscribe to yasgui instance for query event because yasgui channels the event regardless the tab change or creation.
    * Patch the yasgui and the tab implementation to emit a custom yasqeInitialized event because we can't rely on the native tabAbdd, tabChange and tabSelected events due to inconsistent arguments we receive from them and especially the yasqe instance in the `tabChange` event where in certain scenarios it's present but in others it's not yet initialized.
    * Implemented tests.
    
    Removed ts-ignore
    
    renamed yasqeInitialized to yasqeReady

--- cypress/e2e/editor-actions/execute-query.spec.cy.ts
+++ cypress/e2e/editor-actions/execute-query.spec.cy.ts
@@ -2,23 +2,34 @@ import {YasqeSteps} from "../../steps/yasqe-steps";
 import {YasrSteps} from "../../steps/yasr-steps";
 import {QueryStubs} from "../../stubs/query-stubs";
 import ActionsPageSteps from "../../steps/actions-page-steps";
+import {YasguiSteps} from "../../steps/yasgui-steps";
 
 describe('Execute query action', () => {
-    beforeEach(() => {
-        QueryStubs.stubDefaultQueryResponse();
-    });
+  beforeEach(() => {
+    QueryStubs.stubDefaultQueryResponse();
+  });
 
-    it('Should be able to execute a query', () => {
-        ActionsPageSteps.visit();
-        YasqeSteps.getExecuteQueryButton().should('be.visible');
-        YasqeSteps.executeQuery();
-        YasrSteps.getResults().should('have.length', 36);
-    });
+  it('Should be able to execute a query', () => {
+    ActionsPageSteps.visit();
+    YasqeSteps.getExecuteQueryButton().should('be.visible');
+    YasqeSteps.executeQuery();
+    YasrSteps.getResults().should('have.length', 36);
+  });
 
-    it('Should display a progress indicator during query execution', () => {
-        QueryStubs.stubDefaultQueryResponse(2000);
-        ActionsPageSteps.visit();
-        YasqeSteps.executeQuery();
-        YasrSteps.getResults().should('have.length', 36);
-    });
+  it('Should display a progress indicator during query execution', () => {
+    QueryStubs.stubDefaultQueryResponse(2000);
+    ActionsPageSteps.visit();
+    YasqeSteps.executeQuery();
+    YasrSteps.getResults().should('have.length', 36);
+  });
+
+  it('Should emit queryExecuted event on each editor tab', () => {
+    ActionsPageSteps.visit();
+    YasqeSteps.executeQuery();
+    ActionsPageSteps.getEventLog().should('have.value', '\n"select * where {  \\n ?s ?p ?o . \\n } limit 100"');
+    YasguiSteps.openANewTab();
+    ActionsPageSteps.clearEventLog();
+    YasqeSteps.executeQuery(1);
+    ActionsPageSteps.getEventLog().should('have.value', '\n"select * where {  \\n ?s ?p ?o . \\n } limit 100"');
+  });
 });
