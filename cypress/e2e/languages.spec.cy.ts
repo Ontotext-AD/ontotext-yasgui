ba716206a979f5a06e0eed952cb29980d1211e8a
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-03-06 15:59:37 +0200

    Fixes for test "languages.speck.cy.ts"

--- cypress/e2e/languages.spec.cy.ts
+++ cypress/e2e/languages.spec.cy.ts
@@ -6,57 +6,57 @@ import {YasrSteps} from '../steps/yasr-steps';
 
 describe('Languages', () => {
 
-    beforeEach(() => {
-        QueryStubs.stubDefaultQueryResponse();
-        LanguagesSteps.visit();
+  beforeEach(() => {
+    QueryStubs.stubDefaultQueryResponse();
+    LanguagesSteps.visit();
+  });
+
+  describe('Ontotext-yasgui-web-component internationalization support', () => {
+
+    it('Should labels be translated to different languages', () => {
+      // I expect to see the button "editor only" to be translated to the default English language.
+      ToolbarPageSteps.getYasqeModeButton().contains('Editor only');
+      // When the mouse is over the button orientation.
+      ToolbarPageSteps.showLayoutOrientationButtonTooltip();
+      // Then I expect to see the tooltip to be translated to the default English language.
+      cy.contains('Switch to horizontal view');
+
+      // When change the language to be French
+      LanguagesSteps.switchToFr();
+
+      // Then I expect to see the button "editor only" to be translated to French language.
+      ToolbarPageSteps.getYasqeModeButton().contains('Éditeur seulement');
+
+      // When the mouse is over the button orientation.
+      ToolbarPageSteps.showLayoutOrientationButtonTooltip();
+      // Then I expect to see the tooltip to be translated to French language.
+      cy.contains('Basculer vers horizontal voir');
     });
 
-    describe('Ontotext-yasgui-web-component internationalization support', () => {
-
-        it('Should labels be translated to different languages', () => {
-            // I expect to see the button "editor only" to be translated to the default English language.
-            ToolbarPageSteps.getYasqeModeButton().contains('Editor only');
-            // When the mouse is over the button orientation.
-            ToolbarPageSteps.showLayoutOrientationButtonTooltip();
-            // Then I expect to see the tooltip to be translated to the default English language.
-            cy.contains('Switch to horizontal view');
-
-            // When change the language to be French
-            LanguagesSteps.switchToFr();
-
-            // Then I expect to see the button "editor only" to be translated to French language.
-            ToolbarPageSteps.getYasqeModeButton().contains('Éditeur seulement');
-
-            // When the mouse is over the button orientation.
-            ToolbarPageSteps.showLayoutOrientationButtonTooltip();
-            // Then I expect to see the tooltip to be translated to French language.
-            cy.contains('Basculer vers horizontal voir');
-        });
-
-        it('Should translate with translation passed as configuration', () => {
-            ToolbarPageSteps.getYasqeModeButton().contains('Editor only');
-            // When I add external translations
-            LanguagesSteps.addTranslationConfiguration();
-            // I expect to see the button "editor only" to be translated trough label passed as external configuration
-            ToolbarPageSteps.getYasqeModeButton().contains('Editor only passed from external configuration');
-            // When I switch to Bulgarian language passed as external configuration
-            LanguagesSteps.switchToBg();
-            // I expect to see the button "editor only" to be translated to Bulgarian language
-            ToolbarPageSteps.getYasqeModeButton().contains('Само едитор');
-        });
+    it('Should translate with translation passed as configuration', () => {
+      ToolbarPageSteps.getYasqeModeButton().contains('Editor only');
+      // When I add external translations
+      LanguagesSteps.addTranslationConfiguration();
+      // I expect to see the button "editor only" to be translated trough label passed as external configuration
+      ToolbarPageSteps.getYasqeModeButton().contains('Editor only passed from external configuration');
+      // When I switch to Bulgarian language passed as external configuration
+      LanguagesSteps.switchToBg();
+      // I expect to see the button "editor only" to be translated to Bulgarian language
+      ToolbarPageSteps.getYasqeModeButton().contains('Само едитор');
     });
+  });
 
-    describe('Yasr internationalization support', () => {
+  describe('Yasr internationalization support', () => {
 
-        it('Should translate errors', () => {
-            // When execute a query and receive an error.
-            cy.intercept('/repositories/test-repo', {
-                statusCode: 401,
-            });
-            YasqeSteps.executeQuery();
+    it('Should translate errors', () => {
+      // When execute a query and receive an error.
+      cy.intercept('/repositories/test-repo', {
+        statusCode: 401,
+      });
+      YasqeSteps.executeQuery();
 
-            // Then I expect to see error message be translated to English language.
-            YasrSteps.getErrorHeader().contains('Try query in new browser window');
+      // Then I expect to see error message be translated to English language.
+      YasrSteps.getErrorHeader().contains('Try query in new browser window');
 
       // When change the language to be French
       LanguagesSteps.switchToFr();
@@ -64,22 +64,23 @@ describe('Languages', () => {
       // We have to wait a bit because cypress is too fast and grabs the old element (with the old label) and the test fails.
       cy.wait(500);
 
-            // Then I expect to see error message be translated to French language.
-            YasrSteps.getErrorHeader().contains('Essayez la requête dans une nouvelle fenêtre du navigateur');
+      // Then I expect to see error message be translated to French language.
+      YasrSteps.getErrorHeader().contains('Essayez la requête dans une nouvelle fenêtre du navigateur');
 
-        });
+    });
 
-        it('Should translate labels in table plugin', {
-          retries: {
-            runMode: 1,
-            openMode: 0,
-          },
-        },() => {
-            // When execute a query.
-            YasqeSteps.executeQuery();
+    it('Should translate labels in table plugin', {
+      retries: {
+        runMode: 1,
+        openMode: 0,
+      },
+    }, () => {
+      // When execute a query.
+      YasqeSteps.executeQuery();
 
-            // Then I expect to see error message be translated to English language.
-            YasrSteps.getResultFilter().invoke('attr', 'placeholder').should('contain', 'Filter query results');
+      // Then I expect to see messages in table plugin be translated to English language.
+      YasrSteps.getResults().should('be.visible');
+      YasrSteps.getResultFilter().invoke('attr', 'placeholder').should('contain', 'Filter query results');
 
       // When change the language to be French
       LanguagesSteps.switchToFr();
@@ -87,9 +88,10 @@ describe('Languages', () => {
       // We have to wait a bit because cypress is too fast and grabs the old element (with the old label) and the test fails.
       cy.wait(500);
 
-            // Then I expect yasr to be translated to French.
-            YasrSteps.getResultFilter().invoke('attr', 'placeholder').should('contain', 'Filtrer les résultats des requêtes');
+      // Then I expect yasr to be translated to French.
+      YasrSteps.getResultFilter().should('be.visible');
+      YasrSteps.getResultFilter().invoke('attr', 'placeholder').should('contain', 'Filtrer les résultats des requêtes');
 
-        });
     });
+  });
 });
