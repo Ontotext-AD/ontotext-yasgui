4f80b5bc9ba15bb766f54a11c56fed31c35bd375
Svilen Velikov <svilen.velikov@ontotext.com>
2023-03-08 19:07:56 +0200

    Add changes related with the yasgui source code and the new sesame-prefixes autocompleter

--- Yasgui/packages/yasqe/src/index.ts
+++ Yasgui/packages/yasqe/src/index.ts
@@ -1118,6 +1118,7 @@ export interface Config extends Partial<CodeMirror.EditorConfiguration> {
   editorHeight: string;
   queryingDisabled: string | undefined; // The string will be the message displayed when hovered
   prefixCcApi: string; // the suggested default prefixes URL API getter
+  prefixes: string[];
   translationService: TranslationService;
   infer?: boolean;
   sameAs?: boolean;
