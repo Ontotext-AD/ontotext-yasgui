3c22100872163770ded9491499efacd76daa10c6
Svilen Velikov <svilen.velikov@ontotext.com>
2023-03-30 16:05:18 +0300

    Extend raw response yasr plugin and remove the download response button because we already have one.

---
+++ Yasgui/packages/yasr/src/plugins/response/extended-response.ts
@@ -0,0 +1,38 @@
+import Response from "./index";
+import { addClass } from "@triply/yasgui-utils";
+
+export default class ExtendedResponse extends Response {
+    // Function is overriden and download button is removed from the view because we already has one.
+    showLess(setValue = true) {
+        if (!this.cm) return;
+        // Add overflow
+        addClass(this.cm.getWrapperElement(), "overflow");
+
+        // Remove old instance
+        if (this.overLay) {
+            this.overLay.remove();
+            this.overLay = undefined;
+        }
+
+        // Wrapper
+        this.overLay = document.createElement("div");
+        addClass(this.overLay, "overlay");
+
+        // overlay content
+        const overlayContent = document.createElement("div");
+        addClass(overlayContent, "overlay_content");
+
+        const showMoreButton = document.createElement("button");
+        showMoreButton.title = this.translationService.translate("yasr.plugin.response.show_all.btn.label");
+        addClass(showMoreButton, "yasr_btn", "overlay_btn");
+        showMoreButton.textContent = this.translationService.translate("yasr.plugin.response.show_all.btn.label");
+        showMoreButton.addEventListener("click", () => this.showMore());
+        overlayContent.append(showMoreButton);
+
+        this.overLay.appendChild(overlayContent);
+        this.cm.getWrapperElement().appendChild(this.overLay);
+        if (setValue) {
+            this.cm.setValue(this.limitData(this.yasr.results?.getOriginalResponseAsString() || ""));
+        }
+    }
+}
