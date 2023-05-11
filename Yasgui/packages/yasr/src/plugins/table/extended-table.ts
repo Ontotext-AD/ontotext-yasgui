b977827001a7f2fc1f6dd52504452597621efc4f
boyan.tonchev <boyan.tonchev@ontotext.com>
2023-05-11 09:58:47 +0300

    GDB-8183: different results when clicking sort button in yasr result table
    ## What
    - When run query that returns results and click on a results table column to sort them, the results are sorted differently than those in WB;
    - When run query that returns results and click on a results table column to sort them, the numbers of the first column are not consecutive.
    
    ## Why
     - The [DataTables](https://datatables.net/) column configuration has a rendering function defined, called to format data table cells. The result of the function is a string that is used as cell data, sorting, and filtering. When the render function is called for visualisation purpose, data from server is formatted. For example if the data is uri it is transformed from full uri to short uri with prefix. When the function is called for sorting and filtering it returns original data from the server. This is the reason for the difference with the WB. In WB, the same formatting is used for visualisation, sorting, and filtering;
     - The first column is implemented as a simple data column, and when the table is sorted, the first column is rendered according to the sorting algorithm.
    
    ## How
    - The render function is refactored, to returns same result for visualisation, sorting and filtering;
    - The first column configuration is changed according [Official solution from DataTable](https://datatables.net/examples/api/counter_columns.html).

--- Yasgui/packages/yasr/src/plugins/table/extended-table.ts
+++ Yasgui/packages/yasr/src/plugins/table/extended-table.ts
@@ -29,10 +29,64 @@ export class ExtendedTable extends Table {
 
   public draw(persistentConfig: PersistentConfig) {
     super.draw(persistentConfig);
+    this.setupIndexColumn();
     const explainPlanQueryElement = document.getElementById("explainPlanQuery");
     if (!explainPlanQueryElement) {
       return;
     }
     Yasqe.runMode(explainPlanQueryElement.innerText, "sparql11", explainPlanQueryElement);
+  }
+
+  protected getColumns(): DataTables.ColumnSettings[] {
+    if (!this.yasr.results) return [];
+    const prefixes = this.yasr.getPrefixes();
+
+    return [
+      {
+        searchable: false,
+        width: `${this.getSizeFirstColumn()}px`,
+        orderable: false,
+        visible: this.persistentConfig.compact !== true,
+        render: (data: number, type: any) =>
+          type === "filter" || type === "sort" || !type ? data : `<div class="rowNumber">${data}</div>`,
+      }, //prepend with row numbers column
+      ...this.yasr.results?.getVariables().map((name) => {
+        return <DataTables.ColumnSettings>{
+          name: name,
+          title: name,
+          render: (data: Parser.BindingValue | "", type: any, _row: any, _meta: DataTables.CellMetaSettings) => {
+            // Handle empty rows
+            if (data === "") {
+              return data;
+            }
+            if (!type) {
+              return data.value;
+            }
+            return this.getCellContent(data, prefixes);
+          },
+        };
+      }),
+    ];
+  }
+
+  /**
+   * Set-ups first column to be index column according official solution from DataTable.
+   * @see <a href="https://datatables.net/examples/api/counter_columns.html">Official solution from DataTable</a>
+   * @private
+   */
+  private setupIndexColumn() {
+    if (!this.dataTable) {
+      return;
+    }
+    const dataTable = this.dataTable;
+    // Set up first column to be index column according official solution from DataTable.
+    this.dataTable
+      .on("order.dt search.dt", function () {
+        let i = 1;
+        dataTable.cells(null, 0, { search: "applied", order: "applied" }).every(function (cell) {
+          this.data(i++);
+        });
+      })
+      .draw();
   }
 }
