872a424f3d2a52176cbba0fc310b1650bc310b8c
boyantonchev <boyan.tonchev@graphwise.ai>
2025-12-04 16:36:33 +0200

    GDB-13495: Console Errors Occur During Autocomplete in Some SPARQL Editor Scenarios
    ## What
    In certain situations while typing queries in the SPARQL editor, autocomplete triggers a console error. Although autocomplete works in general, the error consistently appears when the suggested value begins with the full URI of a prefix defined at the beginning of the query.
    
    ## Why
    A null pointer exception occurred during processing of autocomplete suggestions.
    The problem was that the filtering logic used two different types of values — prefix labels and full URIs — which caused a mismatch. As a result, prefixFound became undefined.
    
    ## How
    The algorithm has been fixed.
    
    (cherry picked from commit ebcaaa759575739e8d710a0ea6e363065155e09e)

--- cypress/e2e/autocomplete/sesame-autocomplete.spec.cy.ts
+++ cypress/e2e/autocomplete/sesame-autocomplete.spec.cy.ts
@@ -18,4 +18,13 @@ describe('Sesame autocomplete', () => {
     YasqeSteps.writeInEditor('{enter} ?o {}} limit 1');
     YasqeSteps.getQuery().should('eq', 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\nselect * where { ?s rdf:li ?o } limit 1');
   });
+  
+  it('Should set apply a prefix if a suggestion start with a namespace', () => {
+    AutocompleteStubs.stubSesamePrefixesResponse();
+    YasqeSteps.getQuery().should('eq', DEFAULT_SPARQL_QUERY);
+    YasqeSteps.clearEditor();
+    YasqeSteps.writeInEditor('PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\nselect * where {{} ?s r');
+    YasqeSteps.getEditor().find('textarea').type('{alt}{enter}');
+    cy.get('.CodeMirror-hints').should('be.visible');
+  });
 })
